58c745d7f7f9487b50561d0caee1dc83033f36ce
NataMellado <natalia.cmc92@gmail.com>
2025-08-16 16:26:04 -0400

    Añade hotspots personalizados

--- data.js
+++ data.js
@@ -28,8 +28,58 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
-      "infoHotspots": []
+      "linkHotspots": [
+      ],
+
+      "infoHotspots": [
+      ],
+
+       "pinHotspots": [
+ 
+        {
+          "title": "Parque Bicentenario",
+          "icon": "img/hotspots/pin.svg",
+          "yaw": -3.0177308950508532, 
+          "pitch": 0.4250210826012246
+        },
+        {
+          "title": "Museo Ralli",
+          "icon": "img/hotspots/pin.svg",
+          "yaw": -1.0325246864761315, 
+          "pitch": 0.1191787427002069
+        },
+        {
+          "title": "Laguna de las Aves",
+          "icon": "img/hotspots/pin.svg",
+          "yaw": -1.986004410127972, 
+          "pitch": 0.16623083706476116
+        },
+        {
+          "title": "Costanera Center",
+          "icon": "img/hotspots/pin.svg",
+          "yaw": 1.619330349697929, 
+          "pitch": -0.061
+        },
+        {
+          "title": "Museo de la Moda",
+          "icon": "img/hotspots/pin.svg",
+          "yaw": -0.7727426698798734, 
+          "pitch": 0.14298056827285066
+        },
+        {
+          "title": "Mirador Pablo Neruda",
+          "icon": "img/hotspots/pin.svg",
+          "yaw": 2.432016854448497, 
+          "pitch": 0.011418869663264331
+        },
+        {
+          "title": "Mirador Gabriela Mistral",
+          "icon": "img/hotspots/pin.svg",
+          "yaw": 2.2562101467125704, 
+          "pitch": 0.038569823078745657
+        },
+      ]
+      
     }
   ],
   "name": "Project Title",
